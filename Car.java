69189053eee84db397d27e90a32b2fb10f5baad6
Pat <emaneizenga@gmail.com>
2024-09-09 10:00:36 -0500

    we'll seeeeeeeee
    data structures and algorithms

---
+++ Car.java
@@ -0,0 +1,21 @@
+
+public class Car {
+	public String vin;
+	public String make;
+	public String model;
+	public short year;
+	public String color;
+	public double miles;
+	public double price;
+	public short speed;
+	public double direction;
+	public double gpsX;
+	public double gpsY;
+	
+	public int acceleration()
+	{
+		
+		
+		return speed;
+	}
+}
